2fa3d07fcfa14747e304065312935f59bf9664da
Tom Smith <Tom_Esq@Yahoo.com>
2026-02-07 22:19:19 +0000

    Wee bitta refactorin

--- c_programming_lang_book/exercise_1-3/tempconv_ftoc.c
+++ c_programming_lang_book/exercise_1-3/tempconv_ftoc.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+#define     LOWER   0
+#define     UPPER   300
+#define     STEP    20
+
+_print_header()
+{
+    printf("fahr | celc\n");
+    printf("---- | ----\n");
+}
+
 main()
 {
     float fahr, celcius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
+    _print_header();
 
-    printf("fahr | celc\n");
-    printf("---- | ----\n");
-    for (fahr = lower; fahr <= upper; fahr = fahr + step)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
     {
         celcius = (5.0 / 9.0) * (fahr - 32);
         printf("%3.0f  | %6.2f\n", fahr, celcius);
--- c_programming_lang_book/exercise_1-4/tempconv_ctof.c
+++ c_programming_lang_book/exercise_1-4/tempconv_ctof.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+#define     LOWER   0
+#define     UPPER   150
+#define     STEP    10
+
+_print_header()
+{
+    printf("celc | fahr\n");
+    printf("---- | ----\n");
+}
+
 main()
 {
     float fahr, celcius;
-    int lower, upper, step;
 
-    lower = 0;
-    upper = 150;
-    step = 10;
-
-    printf("fahr | celc\n");
-    printf("---- | ----\n");
-    for (celcius = lower; celcius <= upper; celcius = celcius + step)
+    _print_header();
+    
+    for (celcius = LOWER; celcius <= UPPER; celcius = celcius + STEP)
     {
         fahr = (celcius * 1.8) + 32;
         printf("%3.0f | %3.0f\n", celcius, fahr);
